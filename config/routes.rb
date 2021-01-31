7595967899798889ef741aa6e3423551207710b1
SR. MAURICIO <mauriciodurant@gmail.com>
2021-01-30 23:11:45 -0300

    falta correr el server y hacer las pruebas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: %i[registrations sessions passwords]
+  devise_scope :user do
+      post '/signup', to: 'registrations#create'
+      post '/login', to: 'sessions#create'
+      delete '/logout', to: 'sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
